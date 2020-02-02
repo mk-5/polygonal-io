17dc5a338e94f5351b9cfd1e5dd7f356346aa425
mk-5 <mateusz@mk5.pl>
2020-02-02 01:23:25 +0100

    ISSUE-0 [PATCH] pipeline adjustments

--- next-version.sh
+++ next-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-PLUGIN_LATEST_VERSION=$(curl -s https://plugins.gradle.org/m2/pl/mk5/polygonal-architecture/pl.mk5.polygonal-architecture.gradle.plugin/maven-metadata.xml | grep -Po '(?<=<version>)([0-9\.]+(-SNAPSHOT)?)' | sort --version-sort -r | head -n 1)
-NEXT_VERSION=$(./gradlew -q nextVersion)
+export PLUGIN_LATEST_VERSION=$(curl -s https://plugins.gradle.org/m2/pl/mk5/polygonal-architecture/pl.mk5.polygonal-architecture.gradle.plugin/maven-metadata.xml | grep -Po '(?<=<version>)([0-9\.]+(-SNAPSHOT)?)' | sort --version-sort -r | head -n 1)
+export NEXT_VERSION=$(./gradlew -q nextVersion)
 git config user.name "Travis CI"
 git config user.email "builds@travis-ci.com"
 git tag $NEXT_VERSION
