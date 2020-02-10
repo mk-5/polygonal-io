70ad47b17b5cd4663c851c1aa6e65ded0774b5a8
mk-5 <mateusz@mk5.pl>
2020-02-10 22:56:41 +0100

    [MINOR] ISSUE-3 parallel polygons verification

--- plugin-gradle/src/main/groovy/pl/mk5/polygonal/verifytask/PackagesSplitter.groovy
+++ plugin-gradle/src/main/groovy/pl/mk5/polygonal/verifytask/PackagesSplitter.groovy
@@ -8,18 +8,19 @@ import pl.mk5.polygonal.polygons.PackageDefExtension
 class PackagesSplitter {
     Map<File, PackageDefExtension> walkAndSplit(File rootLevel, List<PackageDefExtension> packageDefExtensions) {
         Map<File, PackageDefExtension> defExtensionMap = new HashMap<>()
+        def sourcePackageDefExtensions = new ArrayList<>(packageDefExtensions)
         if (packageDefExtensions.isEmpty()) {
             return defExtensionMap
         }
-        def rootLevelDefIndex = packageDefExtensions.findIndexOf({ packageDef -> packageDef.name.equals('') })
+        def rootLevelDefIndex = sourcePackageDefExtensions.findIndexOf({ packageDef -> packageDef.name.equals('') })
         if (rootLevelDefIndex > -1) {
-            defExtensionMap.put(rootLevel, packageDefExtensions.remove(rootLevelDefIndex))
+            defExtensionMap.put(rootLevel, sourcePackageDefExtensions.remove(rootLevelDefIndex))
         }
         rootLevel.eachFileRecurse(FileType.DIRECTORIES, { dir ->
             def baseName = DirectoryPackageConverter.convert(rootLevel, dir)
-            def packageDefIndex = packageDefExtensions.findIndexOf({ packageDef -> packageDef.name.equals(baseName) })
+            def packageDefIndex = sourcePackageDefExtensions.findIndexOf({ packageDef -> packageDef.name.equals(baseName) })
             if (packageDefIndex > -1) {
-                defExtensionMap.put(dir, packageDefExtensions.remove(packageDefIndex))
+                defExtensionMap.put(dir, sourcePackageDefExtensions.remove(packageDefIndex))
             }
         })
         return defExtensionMap;
